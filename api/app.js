aeaeaf09b122c1911405f7f0671f0d9328d8f840
yesimm-47 <yesimm.err@hotmail.com>
2024-10-20 16:54:01 +0300

    routing altyapısı oluşturuldu

--- api/app.js
+++ api/app.js
@@ -1,13 +1,15 @@
+if(process.env.NODE_ENV != "production")
+  require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +26,8 @@ app.use((req,res,next)=>{
   next()
 })
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', require('./routes/index'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
